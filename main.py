d29f9516b66ad1ae75d26abcefad38b81d4594b8
Mikus Sturmanis <92790899+miksussy@users.noreply.github.com>
2022-09-21 09:12:09 +0100

    Updated Main System
    Now updates real time!

--- main.py
+++ main.py
@@ -9,11 +9,9 @@ def rnder():
   f = open("templates/index.html", "wt")
   f.write(str(request.json))
   f.close()
-  os.system("python restart.py")
-  os.system('kill 1')
   
 @app.route("/", methods=["GET"])
 def hello():
   return render_template('index.html')
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=8080, debug=True)
